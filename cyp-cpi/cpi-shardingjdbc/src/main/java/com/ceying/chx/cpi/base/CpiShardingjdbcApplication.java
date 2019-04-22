1cc1dd589cbc88acf65d8ca0022d249ff37cf29d
fungo-jack <1993212>
2019-04-22 16:22:07 +0800

    增加shardingjdbc

--- cyp-cpi/cpi-shardingjdbc/src/main/java/com/ceying/chx/cpi/base/CpiShardingjdbcApplication.java
+++ cyp-cpi/cpi-shardingjdbc/src/main/java/com/ceying/chx/cpi/base/CpiShardingjdbcApplication.java
@@ -3,8 +3,10 @@ package com.ceying.chx.cpi.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class CpiShardingjdbcApplication {
 
 	public static void main(String[] args) {
--- cyp-cpi/cpi-shardingjdbc/src/main/java/com/ceying/chx/cpi/base/rm/controller/OrderController.java
+++ cyp-cpi/cpi-shardingjdbc/src/main/java/com/ceying/chx/cpi/base/rm/controller/OrderController.java
@@ -3,11 +3,15 @@ package com.ceying.chx.cpi.base.rm.controller;
 import com.ceying.chx.cpi.base.rm.dao.OrderMapper;
 import com.ceying.chx.cpi.base.rm.entity.TOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 功能说明: <br>
@@ -29,18 +33,31 @@ public class OrderController {
     }
 
     @GetMapping("/tOrder")
-    public TOrder tOrder(){
+    @Transactional
+    public List tOrder(){
         TOrder tOrder = new TOrder();
         tOrder.setOrderId(1);
         tOrder.setUserId(1);
         orderMapper.insert(tOrder);
-        return new TOrder();
+        TOrder tOrder1 = new TOrder();
+        tOrder.setOrderId(1);
+        tOrder.setUserId(2);
+        orderMapper.insert(tOrder1);
+        TOrder tOrder2 = new TOrder();
+        tOrder.setOrderId(2);
+        tOrder.setUserId(1);
+        orderMapper.insert(tOrder2);
+        TOrder tOrder3 = new TOrder();
+        tOrder.setOrderId(2);
+        tOrder.setUserId(2);
+        orderMapper.insert(tOrder3);
+        return Arrays.asList(tOrder,tOrder1,tOrder2,tOrder3);
     }
 
     @GetMapping("/tOrder1")
     public TOrder tOrder1(){
         TOrder tOrder = new TOrder();
-        tOrder.setOrderId(2);
+        tOrder.setOrderId(1);
         tOrder.setUserId(2);
         orderMapper.insert(tOrder);
         return new TOrder();
